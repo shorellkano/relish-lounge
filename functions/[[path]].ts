3f7c176435f31e8a54720410163c60bbc83ed638
shorellkano <shorell.kano@gmail.com>
2026-05-28 18:31:09 +0100

    Clean up imports in [[path]].ts
    Remove unused import statement for server module.

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,4 @@
 import { createRequestHandler } from "@tanstack/start-server-core";
-import "../dist/server/server";
-
 export const onRequest = createRequestHandler({
   build: await import("../dist/server/server.js"),
 });
